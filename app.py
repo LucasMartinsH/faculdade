b9e69d89063074b7cbc4e53f9e410a11ff84fceb
Lucas Habitzreuter <lucashabitzreuter@hotmail.com>
2025-04-30 23:15:12 -0300

    Corrige erro de sintaxe em app.py e testa funções corretamente

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-echo "# Meu Projeto" > README.md
-git add .
-git commit -m 
-
-touch app.py
-git add app.py
-git commit -m
-
 def soma(a, b): return a + b
 print(soma(2, 3))
 def subtrai(a, b): return a - b
