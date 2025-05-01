11b235143bfea195597c63bcf9fa4ca056373d2a
Lucas Habitzreuter <lucashabitzreuter@hotmail.com>
2025-04-30 23:50:50 -0300

    Atualiza funções e testes unitários

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-def soma(a, b): return a + b
-print(soma(2, 3))
-def subtrai(a, b): return a - b
-print(subtrai(5, 2))
+def soma(a, b):
+    return a + b
+
+def subtrai(a, b):
+    return a - b
--- test_app.py
+++ test_app.py
@@ -1,11 +1,16 @@
 from app import soma, subtrai
 
-def test_soma():
-
+def test_soma_positivos():
     assert soma(2, 3) == 5
 
-def test_subtrai():
+def test_soma_negativos():
+    assert soma(-1, -2) == -3
 
-    assert subtrai(5, 2) == 3
+def test_subtrai_positivos():
+    assert subtrai(5, 3) == 2
 
+def test_subtrai_negativos():
+    assert subtrai(-5, -3) == -2
 
+def test_soma_zero():
+    assert soma(0, 0) == 0
