c4a3d49cf9df3a055fdbdf89f1daa2fb36378e0e
Lucas Habitzreuter <lucashabitzreuter@hotmail.com>
2025-04-30 22:55:21 -0300

    Adiciona função soma

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-echo "# Meu Projeto" > README.md
-git add .
-git commit -m "Adiciona README inicial"
-
-# Faça mais alterações e commits:
-touch app.py
-git add app.py
-git commit -m "Adiciona estrutura base do app"
-
-# Repita até completar 5 commits.
 def soma(a, b): return a + b
-print(soma(2, 3))
-# Fim do código
