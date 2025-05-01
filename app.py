154cb76e0c581f4639ea86732943d8b1ea3fc385
LucasMartinsH <lucashabitzreuter@hotmail.com>
2025-04-30 22:45:55 -0300

    Adiciona app base

---
+++ app.py
@@ -0,0 +1,10 @@
+echo "# Meu Projeto" > README.md
+git add .
+git commit -m "Adiciona README inicial"
+
+# Faça mais alterações e commits:
+touch app.py
+git add app.py
+git commit -m "Adiciona estrutura base do app"
+
+# Repita até completar 5 commits.
